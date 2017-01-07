22d0c45f65d974c4cb4caa7a4425e984d199f88c
Saideep Chhetri <saideep_chhetri@yahoo.com>
2017-01-07 03:56:51 +0000

    http service n observable used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {ProductService} from './products/product.service'
 @Component({
   selector: 'pm-root',
   template: `
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
         <h1>{{ pageTitle }}</h1>
         <pm-products></pm-products>
     </div>
-     `
-  //,styleUrls: ['./app.component.css']
+     `,
+  //styleUrls: ['./app.component.css'],
+  providers: [ProductService]
+
 })
 export class AppComponent {
   pageTitle: string = 'SRC Product Management';
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
+import {ProductService} from './product.service'
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
@@ -10,35 +11,25 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = '';
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Table",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "Wooden garden table",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/download/230309/garden-table.svg"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0023",
-            "releaseDate": "January 3, 2017",
-            "description": "A hammer for household works",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/download/193438/freehammer.svg"
-        }
-    ];
+    listFilter: string;
+    errorMessage: string;
+    products: IProduct[];
+
+    constructor(private _productService: ProductService){
+
+    }
 
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log("OnInit in action!");
+        this._productService.getProduct()
+               .subscribe(products=> this.products = products,
+                          error=> this.errorMessage = <any>error);
+    }
+
+    onRatingClicked(message: string): void{
+        this.pageTitle = 'Product List: ' + message;
     }
 }
